30129adc97a76629c23ca297091a0af316b3f79e
Chikara Saito <nagulog@gmail.com>
2017-11-03 18:35:36 +0900

    たたき台

--- todoist-auto-scheduler.py
+++ todoist-auto-scheduler.py
@@ -1,30 +1,67 @@
 #-*- coding:utf-8 -*-
 
-# TOKEN = 'api_token' in API_TOKEN.py
+# conf.py contains the Email, Password and Blog's url
+# (e.g.) conf.py
+#   EMAIL = 'example@your.email'
+#   PASSWORD = 'YourPassword123'
+#   TargetURL = 'http://example.com'
 from conf import *
 
-# todoist api
 import todoist
 
-# parser
 import urllib.request
 from bs4 import BeautifulSoup
-import re
 
-if __name__ == '__main__':
-#    api = todoist.TodoistAPI(TOKEN)
+# This function add some tasks to your todoist.
+# The default added task's date is the next day.  
+# tasks get from parse_tasks_from_blog
+# Please use Email and Password from your todoist account 
+def add_task_to_todoist(tasks, Email, Password):
+    # Login to your account
     api = todoist.TodoistAPI()
-    user = api.user.login(EMAIL, PASSWORD)
-    print (user['full_name'])
-    response = api.sync()
-    for project in response['projects']:
-        print(project['name'])
+    api.user.login(Email, Password)
+    api.sync()
 
+    # Add tasks
+    # Default project_id='' is "inbox" project
+    for task_name in tasks:
+        api.items.add(content=task_name, project_id='', date_string='tomorrow')
+    api.commit()
 
-    url = TargetURL
-    html = urllib.request.urlopen(url).read()
+# This function parse tasks from hatenablog.
+# targetURL is http://~~~~ (your blog URL)
+# keyword and mark
+# (e.g.) keyword is 'What to do tomorro', mark is '-', in the blog article
+#       ~~~
+#       What to do tomorrow
+#        - task1
+#        - task2 ...
+# (e.g.) keyword is '明日やること', mark is '・', in the blog article
+#       ~~~
+#       明日やること
+#       ・やること１
+#       ・やること２ ...
+def parse_tasks_from_blog (targetURL, keyword, mark):
+    html = urllib.request.urlopen(targetURL).read()
     soup = BeautifulSoup(html, 'html.parser')
 
-    main_content = soup.title.string
+    # In hatenablog, the body of latest article is tagged with 'entry-content'
+    main_content = soup.find(class_='entry-content')
+    body_text = main_content.get_text().split(mark)
 
-    print(main_content)
+    tasks = []
+    flag = False
+    # tasks variable contains the tasks of next day.
+    for target_task in body_text:
+        if (flag == True):
+            tasks.append(target_task.strip().replace('　', ' '))
+        if ('明日やること' in target_task):
+            flag = True
+    return tasks
+
+if __name__ == '__main__':
+    keyword = '明日やること'
+    mark = '・'
+    
+    tasks = parse_tasks_from_blog(TargetURL, keyword, mark)
+    add_task_to_todoist(tasks, EMAIL, PASSWORD)
