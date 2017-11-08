0e4d01546fb4af0e3416e1bf7fae1e543cbdf611
Chikara Saito <nagulog@gmail.com>
2017-11-08 17:56:02 +0900

    [Fix] データベース中に目的のテーブルが存在するかの判定を関数にまとめる

--- todoist-auto-scheduler.py
+++ todoist-auto-scheduler.py
@@ -77,6 +77,24 @@ def add_task_to_database(table_name, tasks):
         id_num += 1
     conn.commit()
 
+def compare_tables(table1, table2):
+    # table1とtable2の内容が一致している場合はTrueを返す
+    # 結合元のテーブルがタスクの全体集合になるとき，テーブルの内容一致が検出できないため，結合元を交換して２パターンの比較を行う
+    compare_sql1 = 'select * from ' + table1 + ' left outer join ' + table2 + ' on ( ' + table1 + '.task_name = ' + table2 + '.task_name) where ' + table2 + '.task_name is null'
+    compare_sql2 = 'select * from ' + table2 + ' left outer join ' + table1 + ' on ( ' + table2 + '.task_name = ' + table1 + '.task_name) where ' + table1 + '.task_name is null'
+    comparison_result1 = c.execute(compare_sql1).fetchone()
+    comparison_result2 = c.execute(compare_sql2).fetchone()
+
+    comparison_result = comparison_result1 == None and comparison_result2 == None
+    return comparison_result
+
+def is_empty_table(table_name):
+    # テーブルが存在すれば1を，存在しなければ0を返す
+    exist_sql = "select count(*) from sqlite_master where type='table' and name='" + table_name + "';"
+    result = c.execute(exist_sql).fetchone()[0]
+
+    return result
+
 if __name__ == '__main__':
     if (not os.environ.get('EDITOR')):
         os.environ['EDITOR'] = 'vi'
@@ -90,45 +108,15 @@ if __name__ == '__main__':
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
 
-    # テーブルが存在すれば1を，存在しなければ0を返す
-    exist_sql = "select count(*) from sqlite_master where type='table' and name='latest_task';"
-    if (c.execute(exist_sql).fetchone()[0] == 0):
+    if (is_empty_table('latest_task') == 0):
         # 初回起動時には最新記事のタスクをdatabase.dbに登録する
         add_task_to_database('latest_task', tasks)
-
-        for row in (c.execute('select * from latest_task')):
-            print (row)
-
         add_task_to_todoist(tasks, User_info['Email'], User_info['Password'])
     else:
         # 2回目以降の起動時にはlatest_taskと比較してブログ更新の有無を判断する
         add_task_to_database('today', tasks)
 
-        print('latest_task table')
-        for row in (c.execute('select * from latest_task')):
-            print (row)
-
-        print()
-        print('today table')
-        for row in (c.execute('select * from today')):
-            print (row)
-
-        def compare_tables(table1, table2):
-            compare_sql1 = 'select * from ' + table1 + ' left outer join ' + table2 + ' on ( ' + table1 + '.task_name = ' + table2 + '.task_name) where ' + table2 + '.task_name is null'
-            compare_sql2 = 'select * from ' + table2 + ' left outer join ' + table1 + ' on ( ' + table2 + '.task_name = ' + table1 + '.task_name) where ' + table1 + '.task_name is null'
-            comparison_result1 = c.execute(compare_sql1).fetchone()
-            comparison_result2 = c.execute(compare_sql2).fetchone()
-
-            comparison_result = comparison_result1 == None and comparison_result2 == None
-            return comparison_result
-        # 結合元のテーブルがタスクの全体集合になるとき，テーブルの完全一致が検出できないため，結合元を交換して２パターンの比較を行う
-        # compare_sql1 = 'select * from latest_task left outer join today on (latest_task.task_name = today.task_name) where today.task_name is null'
-        # compare_sql2 = 'select * from today left outer join latest_task on (today.task_name = latest_task.task_name) where latest_task.task_name is null'
-
-        # If table1 == table2, compare_table() return True.
-        # if (c.execute(compare_sql1).fetchone() == None and c.execute(compare_sql2).fetchone() == None) :
         if (compare_tables('latest_task', 'today')):
-            # 前回の更新分との差分がNone，ブログが更新されていない場合
             pass
         else:
             add_task_to_todoist(tasks, User_info['Email'], User_info['Password'])
